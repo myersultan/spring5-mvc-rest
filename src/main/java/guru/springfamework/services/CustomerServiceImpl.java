f8067e3839cd4bcaa461b093d80da5818f69f04a
Yersultan Medetov <y.medetov@gmail.com>
2020-05-01 16:10:30 +0600

    customer patch

--- src/main/java/guru/springfamework/services/CustomerServiceImpl.java
+++ src/main/java/guru/springfamework/services/CustomerServiceImpl.java
@@ -66,4 +66,19 @@ public class CustomerServiceImpl implements CustomerService {
         return saveAndReturnDTO(customer);
 
     }
+
+    @Override
+    public CustomerDTO patchCustomer(Long id, CustomerDTO customerDTO) {
+        return customerRepository.findById(id).map( customer -> {
+            if (customerDTO.getFirstName() != null) {
+                customer.setFirstName(customerDTO.getFirstName());
+            }
+
+            if (customerDTO.getLastName() != null) {
+                customer.setLastName(customerDTO.getLastName());
+            }
+
+            return customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+        }).orElseThrow(RuntimeException::new); // TODO: 5/1/20 implement better exception handling;
+    }
 }
